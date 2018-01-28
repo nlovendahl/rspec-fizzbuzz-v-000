23e1114ac0e35a0c9dcd11eb83acef20bbb5ab66
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-28 00:02:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz
-
+def fizzbuzz(x)
+  if x % 3 == 0 && x % 5 == 0
+    puts "FizzBuzz"
+  elsif x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  else
+    puts nil
 end
